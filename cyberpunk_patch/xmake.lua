0c33b63920a9f3a8c96a890da08621cbbafd46c0
yamashi <yamashi@live.com>
2020-12-15 13:15:28 +0100

    Improve build quality

--- cyberpunk_patch/xmake.lua
+++ cyberpunk_patch/xmake.lua
@@ -3,13 +3,15 @@ set_languages("cxx17")
 add_requires("zlib", "spdlog", "nlohmann_json", "mhook")
 
 add_rules("mode.debug", "mode.release")
-add_cxflags("-flto")
-add_ldflags("-flto")
+
+add_ldflags("/LTCG", "/OPT:REF")
+add_cxflags("/Ot", "/GL", "/Ob2", "/Oi", "/GS-")
 
 set_optimize("fastest")
 
 target("performance_overhaul")
     set_kind("shared")
+    set_filename("cyber_engine_tweaks.asi")
     add_files("src/**.cpp")
     add_includedirs("src/")
     add_syslinks("User32", "d3d11")
