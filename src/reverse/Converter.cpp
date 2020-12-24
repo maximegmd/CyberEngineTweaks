961b1a573064770658d36831bc7534bd03ffeb44
yamashi <yamashi@live.com>
2020-12-24 14:47:13 +0100

    Replate std::tuple + recursive templates with folding

--- src/reverse/Converter.cpp
+++ src/reverse/Converter.cpp
@@ -3,100 +3,93 @@
 #include "BasicTypes.h"
 #include "LuaRED.h"
 
-template<int64_t i, typename T, std::enable_if_t<i < 0>* = nullptr>
-sol::object RecursiveInternalToLua(T& arr, RED4ext::REDreverse::CScriptableStackFrame::CStackType& aResult, sol::state_view aLua)
-{
-    return sol::nil;
-}
-
-template<int64_t i, typename T, std::enable_if_t<i >= 0>* = nullptr>
-sol::object RecursiveInternalToLua(T& arr, RED4ext::REDreverse::CScriptableStackFrame::CStackType& aResult, sol::state_view aLua)
-{
-    if (std::get<i>(arr).Is(aResult.type))
-        return std::get<i>(arr).ToLua(aResult, aLua);
-
-    return RecursiveInternalToLua<i - 1>(arr, aResult, aLua);
-}
-
-template<typename T>
-sol::object InternalToLua(T& arr, RED4ext::REDreverse::CScriptableStackFrame::CStackType& aResult, sol::state_view aLua)
-{
-    return RecursiveInternalToLua<std::tuple_size<T>::value - 1>(arr, aResult, aLua);
-}
-
-template<int64_t i, typename T, std::enable_if_t<i < 0>* = nullptr>
-RED4ext::REDreverse::CScriptableStackFrame::CStackType RecursiveInternalToRED(T& arr, sol::object aObject, RED4ext::REDreverse::CRTTIBaseType* apRtti, TiltedPhoques::Allocator* apAllocator)
-{
-    return {};
-}
-
-template<int64_t i, typename T, std::enable_if_t<i >= 0>* = nullptr>
-RED4ext::REDreverse::CScriptableStackFrame::CStackType RecursiveInternalToRED(T& arr, sol::object aObject, RED4ext::REDreverse::CRTTIBaseType* apRtti, TiltedPhoques::Allocator* apAllocator)
-{
-    if (std::get<i>(arr).Is(apRtti))
-        return std::get<i>(arr).ToRED(aObject, apRtti, apAllocator);
-
-    return RecursiveInternalToRED<i - 1>(arr, aObject, apRtti, apAllocator);
-}
-
-template<typename T>
-RED4ext::REDreverse::CScriptableStackFrame::CStackType InternalToRED(T& arr, sol::object aObject, RED4ext::REDreverse::CRTTIBaseType* apRtti, TiltedPhoques::Allocator* apAllocator)
-{
-    return RecursiveInternalToRED<std::tuple_size<T>::value - 1>(arr, aObject, apRtti, apAllocator);
-}
-
-template<int64_t i, typename T, std::enable_if_t<i < 0>* = nullptr>
-size_t RecursiveInternalSize(T& arr, RED4ext::REDreverse::CRTTIBaseType* apRtti)
-{
-    return 0;
-}
-
-template<int64_t i, typename T, std::enable_if_t<i >= 0>* = nullptr>
-size_t RecursiveInternalSize(T& arr, RED4ext::REDreverse::CRTTIBaseType* apRtti)
-{
-    if (std::get<i>(arr).Is(apRtti))
-        return std::get<i>(arr).Size();
-
-    return RecursiveInternalSize<i - 1>(arr, apRtti);
-}
-
-template<typename T>
-size_t InternalSize(T& arr, RED4ext::REDreverse::CRTTIBaseType* apRtti)
-{
-    return RecursiveInternalSize<std::tuple_size<T>::value - 1>(arr, apRtti);
-}
-
-// Note that this be parse from end to beginning, so if you want a type to be parsed first, put it at the end
-static std::tuple<
-    LuaRED<int8_t, "Int8">,
-    LuaRED<int16_t, "Int16">,
-    LuaRED<int32_t, "Int32">,
-    LuaRED<int64_t, "Int64">,
-    LuaRED<uint8_t, "Uint8">,
-    LuaRED<uint16_t, "Uint16">,
-    LuaRED<uint32_t, "Uint32">,
-    LuaRED<uint64_t, "Uint64">,
-    LuaRED<float, "Float">,
-    LuaRED<bool, "Bool">,
-    LuaRED<Quaternion, "Quaternion">,
-    LuaRED<Vector4, "Vector4">,
-    LuaRED<EulerAngles, "EulerAngles">,
-    LuaRED<ItemID, "gameItemID">,
-    LuaRED<TweakDBID, "TweakDBID">,
-    LuaRED<CName, "CName">
-> s_convertersMeta;
+auto s_metaVisitor = [](auto... args) {
+    return [=](auto&& f) mutable { f(args...); };
+}(
+    LuaRED<int8_t, "Int8">(),
+    LuaRED<int16_t, "Int16">(),
+    LuaRED<int32_t, "Int32">(),
+    LuaRED<int64_t, "Int64">(),
+    LuaRED<uint8_t, "Uint8">(),
+    LuaRED<uint16_t, "Uint16">(),
+    LuaRED<uint32_t, "Uint32">(),
+    LuaRED<uint64_t, "Uint64">(),
+    LuaRED<float, "Float">(),
+    LuaRED<bool, "Bool">(),
+    LuaRED<Quaternion, "Quaternion">(),
+    LuaRED<Vector4, "Vector4">(),
+    LuaRED<EulerAngles, "EulerAngles">(),
+    LuaRED<ItemID, "gameItemID">(),
+    LuaRED<TweakDBID, "TweakDBID">(),
+    LuaRED<CName, "CName">()
+    );
 
 size_t Converter::Size(RED4ext::REDreverse::CRTTIBaseType* apRtti)
 {
-    return InternalSize(s_convertersMeta, apRtti);
+    size_t s = 0;
+
+    auto initSize = [&](auto& x)
+    {
+        if (x.Is(apRtti))
+        {
+            s = x.Size();
+            return true;
+        }
+        return false;
+    };
+
+    auto f = [initSize](auto&&... xs)
+    {
+        (... && !initSize(xs));
+    };
+
+    s_metaVisitor(f);
+
+    return s;
 }
 
 sol::object Converter::ToLua(RED4ext::REDreverse::CScriptableStackFrame::CStackType& aResult, sol::state_view aLua)
 {
-    return InternalToLua(s_convertersMeta, aResult, aLua);
+    sol::object o = sol::nil;
+    auto initLuaObject = [&](auto& x)
+    {
+        if (x.Is(aResult.type))
+        {
+            o = x.ToLua(aResult, aLua);
+            return true;
+        }
+        return false;
+    };
+
+    auto f = [initLuaObject](auto&&... xs)
+    {
+        (... && !initLuaObject(xs));
+    };
+
+    s_metaVisitor(f);
+
+    return o;
 }
 
 RED4ext::REDreverse::CScriptableStackFrame::CStackType Converter::ToRED(sol::object aObject, RED4ext::REDreverse::CRTTIBaseType* apRtti, TiltedPhoques::Allocator* apAllocator)
 {
-    return InternalToRED(s_convertersMeta, aObject, apRtti, apAllocator);
+    RED4ext::REDreverse::CScriptableStackFrame::CStackType r;
+    auto initStackType = [&](auto& x)
+    {
+        if (x.Is(apRtti))
+        {
+            r = x.ToRED(aObject, apRtti, apAllocator);
+            return true;
+        }
+        return false;
+    };
+
+    auto f = [initStackType](auto&&... xs)
+    {
+        (... && !initStackType(xs));
+    };
+
+    s_metaVisitor(f);
+
+    return r;
 }
