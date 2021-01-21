888cb5fece92cec62df7ca2212086dba70ed464c
yamashi <yamashi@live.com>
2021-01-21 13:16:42 +0100

    Update xmake for 2.5.1

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,9 @@
-set_languages("cxx17")
+set_languages("cxx20")
 
 add_requires("spdlog", "nlohmann_json", "minhook", "imgui", "sol2", "tiltedcore", {configs = {cxflags = "/DNDEBUG"}, external = false }) -- configs = {cxflags = "/DNDEBUG"} should not be needed when 'debug' is 'false' (default), but for some reason we still pull in debug packages!!!
 
 add_rules("mode.debug","mode.releasedbg", "mode.release")
+add_rules("plugin.vsxmake.autoupdate")
 
 if is_mode("debug") or is_mode("releasedbg") then
     add_defines("CET_DEBUG")
@@ -16,7 +17,7 @@ if is_mode("release") then
     set_optimize("fastest")
 end
 
-add_cxflags("/std:c++latest", "/bigobj", "/MP")
+add_cxflags("/bigobj", "/MP")
 add_defines("RED4EXT_STATIC_LIB", "UNICODE")
 
 before_build(function (target)
@@ -52,7 +53,7 @@ before_build(function (target)
 				end
 			}
 		}
-		
+
 		local cetVersionString = string.format([[
 #pragma once
 
@@ -65,7 +66,7 @@ before_build(function (target)
 				return io.open("src/CETVersion.h", "r")
 			end
 		}
-		if cetVersionCurrent~=nil then 
+		if cetVersionCurrent~=nil then
 			local cetVersionCurrentString = cetVersionCurrent:read("*a")
 			cetVersionCurrent:close()
 			if (cetVersionCurrentString == cetVersionString) then
@@ -77,6 +78,7 @@ before_build(function (target)
 
 target("RED4ext.SDK")
     set_kind("static")
+    set_group("vendor")
     add_files("vendor/RED4ext.SDK/src/**.cpp")
     add_headerfiles("vendor/RED4ext.SDK/include/**.hpp")
     add_includedirs("vendor/RED4ext.SDK/include/", { public = true })
