23cc999173e8236fdf31209121a7fe64a132f1f1
yamashi <yamashi@live.com>
2021-02-16 23:44:13 +0100

    Remove unused code

--- src/scripting/FunctionOverride.h
+++ src/scripting/FunctionOverride.h
@@ -11,13 +11,6 @@ struct FunctionOverride
 
 private:
 
-	struct Overrides
-    {
-        std::string Name;
-        TiltedPhoques::Map<uint64_t, int> Functions;
-    };
-
-    TiltedPhoques::Map<uint64_t, Overrides> m_typeToOverrides;
     void* m_pBufferStart;
     void* m_pBuffer;
     size_t m_size{ 1 << 20 };
