cd6539b45e7d8ea98092505d3d09814a578033ca
Sombra <26506177+Sombra0xCC@users.noreply.github.com>
2021-03-28 09:47:16 +0200

    new TweakDB features and improvements
    • Refactor FlatValuePool
    • Add/Create/Delete record
    • Added GetRecords
    • SetFlat now auto updates the record
    • Added SetFlatNoUpdate for performance if you modify multiple flats for a single record
    • Implicit convert string to TweakDBID

--- src/reverse/TweakDB.h
+++ src/reverse/TweakDB.h
@@ -2,18 +2,57 @@
 
 #include <reverse/BasicTypes.h>
 
+namespace RED4ext
+{
+struct gamedataTweakDBRecord;
+}
+
 struct TweakDB
 {
     TweakDB(const TiltedPhoques::Lockable<sol::state, std::recursive_mutex>::Ref& aLua);
 
     void DebugStats();
+    sol::object GetRecords(const std::string& acRecordTypeName);
+    sol::object GetRecordByName(const std::string& acRecordName);
     sol::object GetRecord(TweakDBID aDBID);
+    sol::object QueryByName(const std::string& acQueryName);
     sol::object Query(TweakDBID aDBID);
+    sol::object GetFlatByName(const std::string& acFlatName);
     sol::object GetFlat(TweakDBID aDBID);
-    bool SetFlat(TweakDBID aDBID, sol::object aValue);
+    bool SetFlatByName(const std::string& acFlatName, sol::object aObject, sol::this_environment aThisEnv);
+    bool SetFlat(TweakDBID aDBID, sol::object aObject, sol::this_environment aThisEnv);
+    bool SetFlatByNameAutoUpdate(const std::string& acFlatName, sol::object aObject, sol::this_environment aThisEnv);
+    bool SetFlatAutoUpdate(TweakDBID aDBID, sol::object aObject, sol::this_environment aThisEnv);
+    bool UpdateRecordByName(const std::string& acRecordName);
     bool UpdateRecordByID(TweakDBID aDBID);
-    bool UpdateRecord(sol::object aValue);
+    bool UpdateRecord(sol::object aValue, sol::this_environment aThisEnv);
+    bool CreateRecord(const std::string& acRecordName, const std::string& acRecordTypeName,
+                      sol::this_environment aThisEnv);
+    bool CloneRecordByName(const std::string& acRecordName, const std::string& acClonedRecordName,
+                           sol::this_environment aThisEnv);
+    bool CloneRecord(const std::string& acRecordName, TweakDBID aClonedRecordDBID, sol::this_environment aThisEnv);
+    bool DeleteRecord(const std::string& acRecordName, sol::this_environment aThisEnv);
 
-private:
+protected:
+    friend struct TweakDBEditor;
+    static int32_t InternalSetFlat(RED4ext::TweakDBID aDBID, const RED4ext::CStackType& acStackType);
+    static bool InternalCreateRecord(const std::string& acRecordName, const std::string& acRecordTypeName,
+                                     std::shared_ptr<spdlog::logger> aLogger = nullptr);
+    static bool InternalCloneRecord(const std::string& acRecordName, RED4ext::TweakDBID aClonedRecordDBID,
+                                    std::shared_ptr<spdlog::logger> aLogger = nullptr);
+    // Can't figure out a good name for this function.
+    // Creates a record of the same type as 'acClonedRecord'
+    // Creates all of its flats
+    // Setting 'cloneValues' to false will set default values
+    static bool InternalCloneRecord(const std::string& acRecordName,
+                                    const RED4ext::gamedataTweakDBRecord* acClonedRecord, bool cloneValues = true,
+                                    std::shared_ptr<spdlog::logger> aLogger = nullptr);
+    static bool InternalDeleteRecord(RED4ext::TweakDBID aDBID, std::shared_ptr<spdlog::logger> aLogger = nullptr);
+    static bool RemoveFlat(RED4ext::TweakDBID aDBID);
+    static bool IsACreatedRecord(RED4ext::TweakDBID aDBID);
+
+private: 
     TiltedPhoques::Lockable<sol::state, std::recursive_mutex>::Ref m_lua;
+    static std::mutex s_mutex;
+    static std::set<RED4ext::TweakDBID> s_createdRecords;
 };
