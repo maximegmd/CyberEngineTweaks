34b100c185349da7665c0ab043572c77bc5fc7e5
yamashi <yamashi@users.noreply.github.com>
2021-09-14 13:54:16 +0200

    1.31

--- src/Image.h
+++ src/Image.h
@@ -6,7 +6,7 @@ struct Image
 
 	static std::tuple<uint32_t, uint16_t> GetSupportedVersion() noexcept
 	{
-        return std::make_tuple(1, 30);
+        return std::make_tuple(1, 31);
 	}
 
     static uint64_t MakeVersion(uint32_t aMajor, uint16_t aMinor) noexcept
