3b3f6b6b6a01fcacb9e18e648107822625fb1e91
yamashi <yamashi@users.noreply.github.com>
2022-11-08 18:36:27 +0100

    Update RenderContext.h

--- src/reverse/RenderContext.h
+++ src/reverse/RenderContext.h
@@ -5,7 +5,7 @@ struct RenderContext
     struct Device
     {
         IDXGISwapChain4* pSwapChain;
-        uint8_t pad8[0x90 - 0x8];
+        uint8_t pad8[0xB0 - 0x8]; // changed size
     };
 
     RenderContext() = delete;
@@ -15,13 +15,10 @@ struct RenderContext
 
     uint8_t pad0[0xC97F38];
     Device devices[0x30]; // Count unknown, it is at least 0x20
-    uint8_t pad[0xED64C0 - (0xC97F38 + sizeof(devices))];
-    void* unkED69C0; // Some pointer, might be related to d3d12on7, I have never seen it being anything other than null
-    uint8_t padED69C8[0x10];
+    uint8_t pad[0xED68D8 - (0xC97F38 + sizeof(devices))];
     ID3D12CommandQueue* pDirectQueue; // ED64D8
 };
 
-static_assert(sizeof(RenderContext::Device) == 0x90);
+static_assert(sizeof(RenderContext::Device) == 0xB0);
 static_assert(offsetof(RenderContext, devices) == 0xC97F38);
-static_assert(offsetof(RenderContext, unkED69C0) == 0xED64C0);
-static_assert(offsetof(RenderContext, pDirectQueue) == 0xED64D8);
+static_assert(offsetof(RenderContext, pDirectQueue) == 0xED68D8);
