ca95e7c6a5a36a069c0ab296ae8399009e274393
maximegmd <672982+maximegmd@users.noreply.github.com>
2024-01-09 15:00:21 +0100

    yoink RED4ext's version checks

--- src/Image.h
+++ src/Image.h
@@ -1,19 +1,17 @@
 #pragma once
 
+#include "RED4ext/Api/v0/FileVer.hpp"
+#include "RED4ext/Api/SemVer.hpp"
+
+
 struct Image
 {
     void Initialize();
 
     static std::tuple<uint32_t, uint16_t> GetSupportedVersion() noexcept { return std::make_tuple(2, 1000); }
 
-    static uint64_t MakeVersion(const uint32_t acMajor, const uint16_t acMinor) noexcept { return static_cast<uint64_t>(acMajor) << 32 | static_cast<uint64_t>(acMinor) << 16; }
-
-    [[nodiscard]] std::tuple<uint32_t, uint16_t> GetVersion() const noexcept
-    {
-        return std::make_tuple(static_cast<uint32_t>(version >> 32), static_cast<uint16_t>((version >> 16) & 0xFFFF));
-    }
-
-    uint64_t version{0};
     uintptr_t base_address;
     mem::region TextRegion;
+    RED4ext::v0::FileVer FileVersion{0,0,0,0};
+    RED4ext::v0::SemVer SemVersion{0, 0, 0, 0};
 };
