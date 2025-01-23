e22a6d27b258957b7810a4789cc87aa2425f23d9
alphanin9 <47785076+alphanin9@users.noreply.github.com>
2025-01-23 17:05:12 +0200

    fix: Patch 2.21 compatibility

--- src/Image.h
+++ src/Image.h
@@ -3,15 +3,14 @@
 #include "RED4ext/Api/v0/FileVer.hpp"
 #include "RED4ext/Api/SemVer.hpp"
 
-
 struct Image
 {
     void Initialize();
 
-    static std::tuple<uint32_t, uint16_t> GetSupportedVersion() noexcept { return std::make_tuple(2, 2000); }
+    static std::tuple<uint32_t, uint16_t> GetSupportedVersion() noexcept { return std::make_tuple(2, 21); }
 
     uintptr_t base_address;
     mem::region TextRegion;
-    RED4ext::v0::FileVer FileVersion{0,0,0,0};
+    RED4ext::v0::FileVer FileVersion{0, 0, 0, 0};
     RED4ext::v0::SemVer SemVersion{0, 0, 0, 0};
 };
